c76ea935f3bfe778c25b5be9347a18b38be0fc41
Stepan Belko <Stpn.Belko@gmail.com>
2023-03-20 15:48:36 +0300

    Added Jasypt plugin. Encrypted all userNames and passwords in application.properties

--- src/main/java/by/itstep/stpnbelko/ItStepFinalAppApplication.java
+++ src/main/java/by/itstep/stpnbelko/ItStepFinalAppApplication.java
@@ -1,9 +1,11 @@
 package by.itstep.stpnbelko;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ItStepFinalAppApplication {
 
     public static void main(String[] args) {
